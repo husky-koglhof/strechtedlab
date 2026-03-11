e33d49c98982438b49d5c74c54e98042cc85444a
Christian Baumgartner <christian.baumgartner@cancom.com>
2026-03-11 05:11:25 +0100

    update router

--- infra/router/main.tf
+++ infra/router/main.tf
@@ -2,11 +2,12 @@ terraform {
   required_providers {
     libvirt = {
       source = "dmacvicar/libvirt"
-      version = "0.8.3"
+      version = "= 0.9.4"
     }
   }
 }
 
 provider "libvirt" {
-  uri = "qemu:///system"
+#  uri = "qemu:///system"
+  uri = "qemu+ssh://root@${var.hypervisor}:22/system?keyfile=${var.private_key_path}"
 }
